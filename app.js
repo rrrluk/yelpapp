baa05a28f6b09225b7ac1e60153a3e7366835074
Pertti Johanson <rrrluk@gmail.com>
2018-07-10 12:46:53 +0300

    post route done

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// MINU VARIABLED
 var appPort = 8006;
+var campgrounds = [{
+        name: "Salmon Creek",
+        image: "https://pixabay.com/get/e83db50a2ff5083ed1584d05fb1d4e97e07ee3d21cac104496f1c07aa6edb2bb_340.jpg"
+    },
+    {
+        name: "Whateva Mountain",
+        image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104496f1c07aa6edb2bb_340.jpg"
+    },
+    {
+        name: "Hipi Meadow",
+        image: "https://pixabay.com/get/e83db50a2ff5083ed1584d05fb1d4e97e07ee3d21cac104496f1c07aa6edb2bb_340.jpg"
+    },
+];
 
 // Landing page
 app.get("/", function (req, res) {
@@ -8,24 +28,31 @@ app.get("/", function (req, res) {
 });
 
 app.get("/campgrounds", function (req, res) {
-    var campgrounds = [{
-            name: "Salmon Creek",
-            image: "https://pixabay.com/get/e83db50a2ff5083ed1584d05fb1d4e97e07ee3d21cac104496f1c07aa6edb2bb_340.jpg"
-        },
-        {
-            name: "Whateva Mountain",
-            image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104496f1c07aa6edb2bb_340.jpg"
-        },
-        {
-            name: "Hipi Meadow",
-            image: "https://pixabay.com/get/e83db50a2ff5083ed1584d05fb1d4e97e07ee3d21cac104496f1c07aa6edb2bb_340.jpg"
-        },
-    ];
     res.render("campgrounds.ejs", {
         campgrounds: campgrounds
     });
+});
+
+
+app.post("/campgrounds", function (req, res) {
+    // get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampGround = {
+        name: name,
+        image: image,
+    }
+    campgrounds.push(newCampGround);
+    // redirect back to campgrounds page
+    res.redirect("/campgrounds");
 
 });
+
+app.get("/campgrounds/new", function (req, res) {
+    res.render("new.ejs");
+});
+
+
 
 // SERVER START
 app.listen(appPort, "localhost", function () {
